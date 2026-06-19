63eaafb662ef1fe478a6b9291528439ac192c4da
kami990 <princetrader9@gmail.com>
2026-06-20 01:36:59 +0500

    Replace old code

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ownerNumber: process.env.OWNER_NUMBER || '923417972691',
+    botName: process.env.BOT_NAME || 'Kami Bot',
+    ownerName: process.env.OWNER_NAME || 'Error'
+};
